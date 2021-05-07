4fc3b2dc363cae50952e9b4de3ca9fec7ebe1cc0
Garrett Weems <gwgraphicdesign@gmail.com>
2021-05-07 16:51:07 -0500

    data.py

---
+++ data/data.py
@@ -0,0 +1,105 @@
+# %%
+import json
+import os
+from glob import glob
+
+
+# from pymongo import MongoClient
+# user = "buffalographics"
+
+# col = "buffalographics"
+# uri = f"mongodb+srv\\[:]\\{user}\\//:Bgsince21@cluster0.z7wmc.mongodb.net/{col}"
+# client = MongoClient(uri)
+
+# db = client.buffalographics
+
+# clients_collection = db.clients
+
+clients_dir = "/Volumes/GoogleDrive/Shared drives/Buffalo Graphics/clients"
+
+
+def build_clients(dir):
+    data_store = 'data.json'
+    clients = {}
+    if os.path.isfile(data_store):
+        with open(data_store) as json_file:
+            clients = json.load(json_file)['clients']
+    else:
+        with open(data_store, 'w') as outfile:
+            json.dump({'clients': {}}, outfile)
+
+    for client in os.listdir(dir):
+        client_dir = os.path.join(dir, client)
+
+        if client not in clients.keys() and os.path.isdir(client_dir):
+
+            clients[client] = {'name': client}
+
+    with open(data_store, 'w') as outfile:
+        json.dump({'clients': clients}, outfile)
+
+    print(clients)
+
+
+build_clients(clients_dir)
+
+
+# def builder(path):
+#     with open(data_store) as json_file:
+#         clients = json.load(json_file)
+
+#     paths = listdir(path)
+#     obj = {}
+#     for item in paths:
+#         i = 0
+#         jobs = {}
+#         dir = os.path.join(path, item)
+#         _jobs = glob(dir+'*/**/*/PRINT', recursive=True)
+#         default = glob(dir+'/PRINT/*.pdf', recursive=True)
+#         if len(default) > 0:
+#             files = {}
+#             for file in default:
+#                 files[file.split('/').pop()] = create_file_obj(file)
+
+#             jobs['.'] = {'files': files}
+#         # print(_jobs)
+#         for job in _jobs:
+#             job_full_path = job
+#             job = job.replace(dir+'/', '').replace('/PRINT', '')
+#             files = {}
+
+#             for file in glob(job_full_path+'/*.pdf'):
+#                 file_obj = create_file_obj(file)
+
+#                 # print(int(qty)
+#                 files[file_obj['file_name']] = file_obj
+
+#             jobs[job] = {
+#                 'full_path': job_full_path,
+#                 'files': files
+#             }
+
+#         obj[item] = {
+#             'full_path': dir,
+#             'jobs': jobs
+
+#         }
+#     return obj
+
+
+# clients = builder(path)
+# print(clients)
+
+# %%
+
+# %%
+
+# %%
+
+# %%
+
+# %%
+
+# %%
+
+# %%
---
+++ data/util.py
@@ -0,0 +1,66 @@
+import os
+from pdfrw import PdfReader
+import json
+
+
+def is_json(myjson):
+    try:
+        json.loads(myjson)
+    except ValueError as e:
+        print(e)
+        return False
+    return True
+
+
+def convert_bytes(num):
+    """
+    this function will convert bytes to MB.... GB... etc
+    """
+    for x in ['bytes', 'KB', 'MB', 'GB', 'TB']:
+        if num < 1024.0:
+            return "%3.1f %s" % (num, x)
+        num /= 1024.0
+
+
+def file_size(file_path):
+    """
+    this function will return the file size
+    """
+    if os.path.isfile(file_path):
+        file_info = os.stat(file_path)
+        return convert_bytes(file_info.st_size)
+
+
+def pdf_dim(file):
+    if os.path.isfile(file) and file.endswith('.pdf'):
+        sizes = {'height': '', 'width': ''}
+        try:
+            media_box = PdfReader(file).pages[0].MediaBox
+            # [0, 0, height, width] in points
+
+            h = (float(media_box[2]) / 72).__round__()
+            w = (float(media_box[3]) / 72).__round__()
+
+            sizes['height'] = f"{h}in"
+            sizes['width'] = f"{w}in"
+
+        except ValueError as e:
+            print(e)
+            print(f"Error getting dimensions of file\n{file}")
+
+        return sizes
+
+
+def create_file_obj(file):
+    sizes = pdf_dim(file)
+
+    file_obj = {
+        'full_path': file,
+        'file_name': file.split('/').pop(),
+        'width': sizes['width'],
+        'height': sizes['height'],
+        'size': file_size(file),
+        'qty': ''
+    }
+
+    return file_obj
