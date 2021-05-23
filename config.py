c5b18ffb0afe20aa5880530371b06a51f623cf83
Garrett Weems <gwgraphicdesign@gmail.com>
2021-05-23 06:34:31 -0500

    :construction: get config vars and check for missing vals

---
+++ config.py
@@ -0,0 +1,40 @@
+# %%
+from os import environ
+from typing import TypedDict
+
+from dotenv import load_dotenv
+
+
+class Config(TypedDict):
+    db_user: str
+    db_password: str
+    db_host: str
+    db_name: str
+    clients_dir_path: str
+
+
+def config() -> Config:
+    load_dotenv()
+
+    vals = (
+        environ.get("DB_USER"),
+        environ.get("DB_PASSWORD"),
+        environ.get("DB_HOST"),
+        environ.get("DB_NAME"),
+        environ.get("CLIENTS_DIR_PATH"),
+    )
+
+    [db_user, db_password, db_host, db_name, clients_dir_path] = vals
+
+    if all(isinstance(item, str) for item in vals):
+
+        return {
+            "db_user": db_user,
+            "db_password": db_password,
+            "db_host": db_host,
+            "db_name": db_name,
+            "clients_dir_path": clients_dir_path,
+        }
+
+
+print(config())
